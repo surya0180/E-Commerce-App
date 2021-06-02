bc1de482988041bbccce996372730af081381cd2
surya0180 <teja8551@gmail.com>
2021-06-02 18:43:01 +0530

    Added error handling for authentication

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
+
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
@@ -8,30 +11,36 @@ class Auth with ChangeNotifier {
   String _userId;
 
   Future<void> _authenticate(
-    String email,
-    String password,
-    String urlSegment,
-  ) async {
+      String email, String password, String urlSegment) async {
     final url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyAWoomRp20bkGreVpIHQyfcfj5V3ltIZ9M');
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': email,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+        ('https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyAWoomRp20bkGreVpIHQyfcfj5V3ltIZ9M'));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      // print(responseData);
+      if (responseData['error'] != null) {
+        // print("I am herr");
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      return Future.error(error);
+    }
   }
 
   Future<void> signup(String email, String password) async {
-    return _authenticate(email, password, "signup");
+    return _authenticate(email, password, 'signUp');
   }
 
   Future<void> login(String email, String password) async {
-    return _authenticate(email, password, "signInWithPassword");
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
